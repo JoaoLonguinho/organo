4d0ca9f44e1034335dec9be753af648b053e50f7
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-07-21 20:29:07 -0300

    Criando campo select com options utilizando .map

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,7 +1,14 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
 const Formulario = () => {
+
+    const times = [
+        'Front-end',
+        'Back-end',
+        'DBA'
+    ]
     return (
         <section className='formulario'>
             <form>
@@ -9,6 +16,7 @@ const Formulario = () => {
                 <CampoTexto label="Nome" ph="Digite seu nome" />
                 <CampoTexto label="Cargo" ph="Digite seu cargo" />
                 <CampoTexto label="Imagem" ph="Faça o upload de sua imagem" />
+                <ListaSuspensa label="Time" itens={times}/>
             </form>
         </section>
     )
