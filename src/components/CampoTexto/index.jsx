318de420564a4974038fe99ab4f8b1df3401886a
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-07-22 19:34:59 -0300

    Utilizando primeiro hook

--- src/components/CampoTexto/index.jsx
+++ src/components/CampoTexto/index.jsx
@@ -1,15 +1,22 @@
 
+import { useState } from 'react'
 import './CampoTexto.css'
 
 const CampoTexto = (props) => { // Feito de forma diferente, utilizando arrow function
     // console.log(props.label)
+
+    // let valorDigitado = 'Uffa'
+
+    const [valorDigitado, setValorDigitado] = useState('teste') // hook
+
     const aoDigitar = (event) => {
-        console.log(event.target.value)
+        setValorDigitado(event.target.value);
+        console.log(valorDigitado)
     }
     return (
         <div className="campo-texto">
             <label htmlFor={props.label}>{props.label}</label>
-            <input type="text" onChange={aoDigitar} required={props.obrigatorio} placeholder={props.ph} name={props.label}></input>
+            <input value={valorDigitado} type="text" onChange={aoDigitar} required={props.obrigatorio} placeholder={props.ph} name={props.label}></input>
         </div>
     )
 }
