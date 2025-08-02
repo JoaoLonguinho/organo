8617e766d5a7f1cc064d3b002bb3be8dd182bd49
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-08-01 22:07:57 -0300

    Criando componente colaborador

---
+++ src/components/Colaborador/index.jsx
@@ -0,0 +1,18 @@
+import "./Colaborador.css";
+
+const Colaborador = (props) =>{
+    return(
+        <div className="colaborador">
+            <div className="cabeçalho">
+                <img src="https://github.com/JoaoLonguinho.png"/>
+            </div>
+            <div className="rodape">
+                <h4>João Longuinho</h4>
+                <h5>Programador full stack</h5>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Colaborador
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,9 +1,11 @@
+import Colaborador from "../Colaborador";
 import "./Time.css";
 
 const Time = (props) =>{
     return(
         <section className="time" style={{background: props.corPrimaria}}>
             <h3 style={{borderColor: props.corSecundaria}}>{props.nomeDoTime}</h3>
+            <Colaborador />
         </section>
     )
 }
