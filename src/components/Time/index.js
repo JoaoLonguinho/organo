320e10f82c2a46f364d90ecc628d9cd9eaeabd63
João Pedro Caramante Longuinho <49046891+JoaoLonguinho@users.noreply.github.com>
2025-08-04 21:23:15 -0300

    Escondendo times vazios

--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -3,7 +3,7 @@ import "./Time.css";
 
 const Time = (props) => {
     return (
-        <section className="time" style={{ background: props.corPrimaria }}>
+        props.colaboradores.length > 0 && <section className="time" style={{ background: props.corPrimaria }}>
             <h3 style={{ borderColor: props.corSecundaria }}>{props.nomeDoTime}</h3>
             <div className="colaboradores">
                 {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />)}
